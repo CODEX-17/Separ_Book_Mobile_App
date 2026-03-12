9393f0d4236d942ea046450f2144406e16140930
RUMAR C. PAMPARO <112467114+CODEX-17@users.noreply.github.com>
2026-03-12 19:19:50 +0800

    change to static feast list data for 2026

--- app/data/feastDateList.ts
+++ app/data/feastDateList.ts
@@ -27,56 +27,56 @@ const banalNaPagtitipon = () => {
   return date.toISOString().split("T")[0]; // Returns ISO format (YYYY-MM-DD)
 };
 
+//This feast is for 2026 only
 export const feast = [
   {
     name: "Feast of Passover",
-    date: new Date(`${year}-01-14`),
-    dateSting: `${year}-01-14`,
+    date: new Date(`2026-02-01`),
+    dateSting: `2026-02-01`,
   },
   {
     name: "Feast of Unleavened bread",
-    date: new Date(`${year}-01-15`),
-    dateSting: `${year}-01-15`,
+    date: new Date(`2026-02-02`),
+    dateSting: `2026-02-02`,
   },
   {
     name: "Feast of Harvest",
-    date: new Date(getFeastOfHarvest()),
-    dateSting: getFeastOfHarvest(),
+    date: new Date(`2026-03-29`),
+    dateSting: `2026-03-29`,
   },
   {
     name: "Feast of Weeks",
-    date: new Date(getFeastOfHarvest()),
-    dateSting: getFeastOfHarvest(),
+    date: new Date(`2026-03-29`),
+    dateSting: `2026-03-29`,
   },
   {
     name: "Feast of Trumpets",
-    date: new Date(`${year}-07-01`),
-    dateSting: `${year}-07-01`,
+    date: new Date(`2026-07-14`),
+    dateSting: `2026-07-14`,
   },
-  //July 9-10
   {
     name: "Day of atonement",
-    dateFrom: new Date(`${year}-07-09`),
-    dateTo: new Date(`${year}-07-10`),
-    dateStringFrom: `${year}-07-09`,
-    dateStringTo: `${year}-07-10`,
+    dateFrom: new Date(`2026-07-24`),
+    dateTo: new Date(`2026-07-25`),
+    dateStringFrom: `2026-07-24`,
+    dateStringTo: `2026-07-25`,
   },
   {
     name: "Feast of Tabernacles",
-    date: new Date(`${year}-07-15`),
-    dateSting: `${year}-07-09`,
+    date: new Date(`2026-07-29`),
+    dateSting: `2026-07-29`,
   },
   {
     name: "Banal na pagtitipon",
-    date: new Date(banalNaPagtitipon()),
-    dateSting: banalNaPagtitipon(),
+    date: new Date(`2026-08-07`),
+    dateSting: `2026-08-07`,
   },
 ];
 
 // Feast of passover Jan 14
 // Jan 14 + 1day = feast of unleavened
 // Jan 14 + 7days + 7 shabbath + 1 day = feast of weeks
-// July 1 = feast of trumpets
+// July 1 = feast of trumpets 
 // July 9-10 = Day of Atonement
 // July 15 = feast of tabernacles
 // July 15 + 8days = banal na pag titipon
